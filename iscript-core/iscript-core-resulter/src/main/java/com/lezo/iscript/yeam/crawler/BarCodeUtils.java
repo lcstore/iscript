f0caddb284fa198041eaeb4769cb0d77e06ea180
lezo <lcstore@126.com>
2014-07-11 19:23:08 +0800

    update barCodeUtils

---
+++ iscript-core/iscript-core-resulter/src/main/java/com/lezo/iscript/yeam/crawler/BarCodeUtils.java
@@ -0,0 +1,38 @@
+package com.lezo.iscript.yeam.crawler;
+
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BarCodeUtils {
+	private static final int BAR_CODE_LEN = 13;// EAN-13
+
+	public static boolean isBarCode(String barCode) {
+		if (StringUtils.isEmpty(barCode)) {
+			return false;
+		}
+		barCode = barCode.trim();
+		if (barCode.length() != BAR_CODE_LEN) {
+			return false;
+		}
+		if (!barCode.matches("[0-9]{13}")) {
+			return false;
+		}
+		int oddSum = 0;// 奇数
+		int evenSum = 0;// 偶数
+		for (int i = 0; i < BAR_CODE_LEN - 1; i++) {
+			if (i % 2 == 0) {
+				oddSum += Integer.valueOf("" + barCode.charAt(i));
+			} else {
+				evenSum += Integer.valueOf("" + barCode.charAt(i));
+			}
+		}
+		int sum = evenSum * 3 + oddSum;
+		int verifyCode = 10 - sum % 10;
+		verifyCode = verifyCode % 10;
+		return verifyCode == Integer.valueOf("" + barCode.charAt(BAR_CODE_LEN - 1));
+	}
+
+}
---
+++ iscript-core/iscript-core-resulter/src/test/java/com/lezo/iscript/yeam/crawler/BarCodeUtilsTest.java
@@ -0,0 +1,33 @@
+package com.lezo.iscript.yeam.crawler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BarCodeUtilsTest {
+	@Test
+	public void testIsBarCode() {
+		Assert.assertEquals(true, BarCodeUtils.isBarCode("9771671216014"));
+		Assert.assertEquals(false, BarCodeUtils.isBarCode("6925303722563"));
+		Assert.assertEquals(true, BarCodeUtils.isBarCode("6925303722562"));
+		Assert.assertEquals(false, BarCodeUtils.isBarCode("692530B722562"));
+		Assert.assertEquals(false, BarCodeUtils.isBarCode("925303722562"));
+		Assert.assertEquals(true, BarCodeUtils.isBarCode("8801111186100"));
+	}
+
+	@Test
+	public void testCheckBarCode() throws Exception {
+		List<String> lineList = FileUtils.readLines(new File("src/test/resources/barCode.txt"));
+		for (String line : lineList) {
+			String[] splitStrings = line.split("\t");
+			if (!BarCodeUtils.isBarCode(splitStrings[1])) {
+				System.out.println(splitStrings[0]);
+			}
+		}
+	}
+
+}
