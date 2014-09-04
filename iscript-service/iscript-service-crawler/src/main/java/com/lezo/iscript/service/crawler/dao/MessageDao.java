d2d1e409562afd5c964dd872200fee9b975b14a0
dlinked <dlinked@126.com>
2014-09-05 01:03:56 +0800

    add message

---
+++ iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/dao/MessageDao.java
@@ -0,0 +1,17 @@
+package com.lezo.iscript.service.crawler.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.lezo.iscript.common.BaseDao;
+import com.lezo.iscript.service.crawler.dto.MessageDto;
+
+public interface MessageDao extends BaseDao<MessageDto> {
+
+	List<MessageDto> getMessageDtos(@Param(value = "type") String type, @Param(value = "status") Integer status,
+			@Param(value = "limit") Integer limit);
+
+	void batchUpdateStatus(@Param(value = "idList") List<Long> idList, @Param(value = "status") Integer status,
+			@Param(value = "handler") String handler);
+}
---
+++ iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/dto/MessageDto.java
@@ -0,0 +1,5 @@
+package com.lezo.iscript.service.crawler.dto;
+
+public class MessageDto {
+
+}
