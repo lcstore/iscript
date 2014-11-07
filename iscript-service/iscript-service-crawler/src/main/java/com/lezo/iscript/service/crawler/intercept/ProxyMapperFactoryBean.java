25cebecb8f3e01ec09dd306e56848f6af36955a4
lezo <lcstore@126.com>
2014-11-07 19:10:56 +0800

    udpate auto batch

--- iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/intercept/ProxyMapperFactoryBean.java
+++ iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/intercept/ProxyMapperFactoryBean.java
@@ -3,12 +3,11 @@ package com.lezo.iscript.service.crawler.intercept;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -18,55 +17,42 @@ public class ProxyMapperFactoryBean<T> extends MapperFactoryBean<T> implements I
 	public static final String DEFAULT_INTERCEPT_METHOD = "batchUpdate";
 	private SqlSessionFactory sqlSessionFactory;
 	private T targetObject;
-	private List<BatchMethod> batchMethods;
+	private List<String> invokParams;
+	private Map<String, String> methodParamMap;
+
+	public ProxyMapperFactoryBean() {
+		methodParamMap = new HashMap<String, String>();
+		methodParamMap.put("batchUpdate", "param1");
+	}
+
+	public boolean isIntercepter(Method method) {
+		return methodParamMap.containsKey(method.getName());
+	}
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		BatchMethod batchMethod = findBatchMethod(method);
-		if (batchMethod != null) {
-			return doIntercepter(proxy, method, args, batchMethod);
+		if (isIntercepter(method)) {
+			return doIntercepter(proxy, method, args);
 		}
 		return method.invoke(this.targetObject, args);
 	}
 
-	private BatchMethod findBatchMethod(Method method) {
-		if (CollectionUtils.isEmpty(batchMethods)) {
-			return null;
-		}
-		Class<?>[] pTypes = method.getParameterTypes();
-		if (pTypes == null || pTypes.length < 1) {
-			return null;
-		}
-		int pSize = pTypes.length;
-		String name = method.getName();
-		for (BatchMethod bm : batchMethods) {
-			if (name.equals(bm.getName()) && pSize > bm.getIndex()) {
-				return bm;
-			}
-		}
-		return null;
-	}
-
-	public Object doIntercepter(Object proxy, Method method, Object[] args, BatchMethod batchMethod) throws Exception {
-		List<?> dataList = (List<?>) args[0];
+	@SuppressWarnings("unchecked")
+	public Object doIntercepter(Object proxy, Method method, Object[] args) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false);
+		Integer result = 0;
 		try {
 			String sqlMapper = getObjectType().getName() + "." + method.getName();
-			Map<String, Object> paramMap = new HashMap<String, Object>();
-			paramMap = getCommonParamMap(method, args, batchMethod);
-			boolean hasParamName = !CollectionUtils.isEmpty(keyList);
-			if (hasParamName) {
-				for (int i = 1; i < keyList.size(); i++) {
-					paramMap.put(keyList.get(i), args[i]);
-				}
+			Object paramObject = SqlSessionParamsUtils.doConvert(method, args);
+			if (!(paramObject instanceof Map)) {
+				throw new RuntimeException("It is not an auto batch mapper:[" + sqlMapper + "]");
 			}
-			for (Object data : dataList) {
-				if (!hasParamName) {
-					sqlSession.update(sqlMapper, data);
-				} else {
-					paramMap.put(keyList.get(0), data);
-					sqlSession.update(sqlMapper, paramMap);
-				}
+			Map<String, Object> paramMap = (Map<String, Object>) paramObject;
+			String batchKey = methodParamMap.get(method.getName());
+			Collection<Object> batchCollection = (Collection<Object>) paramMap.get(batchKey);
+			for (Object bObject : batchCollection) {
+				paramMap.put(batchKey, bObject);
+				result += sqlSession.update(sqlMapper, paramMap);
 			}
 			sqlSession.commit();
 		} catch (Exception ex) {
@@ -75,23 +61,7 @@ public class ProxyMapperFactoryBean<T> extends MapperFactoryBean<T> implements I
 		} finally {
 			sqlSession.close();
 		}
-		return null;
-	}
-
-	private Map<String, Object> getCommonParamMap(Method method, Object[] args, BatchMethod batchMethod) {
-		if (args == null) {
-			return null;
-		}
-		Type[] pTypes = method.getGenericParameterTypes();
-		int batchIndex = batchMethod.getIndex();
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		for (int i = 0; i < pTypes.length; i++) {
-			if (batchIndex != i) {
-				Type pType = pTypes[i];
-				paramMap.put(key, value);
-			}
-		}
-		return paramMap;
+		return result;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -107,12 +77,19 @@ public class ProxyMapperFactoryBean<T> extends MapperFactoryBean<T> implements I
 		super.setSqlSessionFactory(sqlSessionFactory);
 	}
 
-	public List<BatchMethod> getBatchMethods() {
-		return batchMethods;
+	public List<String> getInvokParams() {
+		return invokParams;
 	}
 
-	public void setBatchMethods(List<BatchMethod> batchMethods) {
-		this.batchMethods = batchMethods;
+	public void setInvokParams(List<String> invokParams) {
+		methodParamMap.clear();
+		for (String mParam : invokParams) {
+			int index = mParam.indexOf(":");
+			String methodName = mParam.substring(0, index);
+			String convertParam = mParam.substring(index + 1);
+			methodParamMap.put(methodName, convertParam);
+		}
+		this.invokParams = invokParams;
 	}
 
 }
---
+++ iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/intercept/SqlSessionParamsUtils.java
@@ -0,0 +1,75 @@
+package com.lezo.iscript.service.crawler.intercept;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.binding.MapperMethod.ParamMap;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+/**
+ * @author lezo
+ * @email lcstore@126.com
+ * @since 2014年11月7日
+ */
+public class SqlSessionParamsUtils {
+
+	public static Object doConvert(Method method, Object[] args) {
+		Map<Integer, String> annotationNameMap = getParamNameFromAnnotation(method);
+		SortedMap<Integer, String> params = getParams(method, annotationNameMap);
+		final int paramCount = params.size();
+		if (args == null || paramCount == 0) {
+			return null;
+		} else if (annotationNameMap.isEmpty() && paramCount == 1) {
+			return args[0];
+		} else {
+			final Map<String, Object> param = new ParamMap<Object>();
+			int i = 0;
+			for (Map.Entry<Integer, String> entry : params.entrySet()) {
+				param.put(entry.getValue(), args[entry.getKey()]);
+				// issue #71, add param names as param1, param2...but ensure backward compatibility
+				final String genericParamName = "param" + String.valueOf(i + 1);
+				if (!param.containsKey(genericParamName)) {
+					param.put(genericParamName, args[entry.getKey()]);
+				}
+				i++;
+			}
+			return param;
+		}
+	}
+
+	private static Map<Integer, String> getParamNameFromAnnotation(Method method) {
+		Map<Integer, String> paramNameMap = new HashMap<Integer, String>();
+		final Object[][] annosArray = method.getParameterAnnotations();
+		for (int i = 0; i < annosArray.length; i++) {
+			final Object[] paramAnnos = method.getParameterAnnotations()[i];
+			for (Object paramAnno : paramAnnos) {
+				if (paramAnno instanceof Param) {
+					String paramName = ((Param) paramAnno).value();
+					paramNameMap.put(i, paramName);
+					break;
+				}
+			}
+		}
+		return paramNameMap;
+	}
+
+	private static SortedMap<Integer, String> getParams(Method method, Map<Integer, String> annotationNameMap) {
+		final SortedMap<Integer, String> params = new TreeMap<Integer, String>();
+		final Class<?>[] argTypes = method.getParameterTypes();
+		for (int i = 0; i < argTypes.length; i++) {
+			if (!RowBounds.class.isAssignableFrom(argTypes[i]) && !ResultHandler.class.isAssignableFrom(argTypes[i])) {
+				String paramName = String.valueOf(params.size());
+				if (annotationNameMap.containsKey(i)) {
+					paramName = annotationNameMap.get(i);
+				}
+				params.put(i, paramName);
+			}
+		}
+		return params;
+	}
+}
