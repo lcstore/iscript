fea7811fd5eee34ce97788e29cbb5d7d75f839c7
lcstore <lcstore@126.com>
2014-11-24 01:30:28 +0800

    change client params

--- iscript-core/iscript-core-client/src/main/java/com/lezo/iscript/yeam/task/TasksCaller.java
+++ iscript-core/iscript-core-client/src/main/java/com/lezo/iscript/yeam/task/TasksCaller.java
@@ -5,16 +5,13 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.lezo.iscript.yeam.ClientConstant;
-
 public class TasksCaller {
-	private static final int corePoolSize = 2;
+	private static final int corePoolSize = 5;
 	private static final int maximumPoolSize = 5;
 	private static final long keepAliveTime = 60 * 1000;
-	private static final int capacity = ClientConstant.MAX_TASK_QUEUE_CAPACITY;
+	private static final int capacity = 50;
 	private static final BlockingQueue<Runnable> resultQueue = new ArrayBlockingQueue<Runnable>(capacity);
-	private final ThreadPoolExecutor caller = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime,
-			TimeUnit.MILLISECONDS, resultQueue);
+	private final ThreadPoolExecutor caller = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS, resultQueue);
 
 	private TasksCaller() {
 	}
