40434f2aa804c333ab4a1e8d3f28efc8b12080fa
lcstore <lcstore@126.com>
2014-12-19 01:35:01 +0800

    add save brand

--- iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/dao/BrandStoreDao.java
+++ iscript-service/iscript-service-crawler/src/main/java/com/lezo/iscript/service/crawler/dao/BrandStoreDao.java
@@ -1,8 +1,16 @@
 package com.lezo.iscript.service.crawler.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.lezo.iscript.common.BaseDao;
 import com.lezo.iscript.service.crawler.dto.BrandStoreDto;
 
 public interface BrandStoreDao extends BaseDao<BrandStoreDto> {
 	void insertBrandStoreDtoAndSetId(BrandStoreDto dto);
+
+	List<BrandStoreDto> getBrandStoreDtoByCodes(@Param("brandCodeList")List<String> brandCodeList,@Param("brandNameList") List<String> brandNameList, @Param("siteId")Integer siteId);
+
+	List<BrandStoreDto> getBrandStoreDtoByIds(@Param("idList")List<Long> idList);
 }
